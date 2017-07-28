1e7acb5890a54ef4d374f054273a7976fa068317
Toby Hamand <tobyhamand@gmail.com>
2017-07-28 15:24:08 +0100

    fixing tests

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -7,4 +7,3 @@ require_relative 'models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_development")
 DataMapper.finalize
-DataMapper.auto_upgrade!
